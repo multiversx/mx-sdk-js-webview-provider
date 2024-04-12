585764f09a781c73d06f9a5aa76306e5dbd250ef
cipriandraghici <cipriandraghici93@gmail.com>
2024-04-12 13:23:40 +0300

    provider improvements

--- src/WebviewProvider.ts
+++ src/WebviewProvider.ts
@@ -1,25 +1,25 @@
-import { SignableMessage } from "@multiversx/sdk-core/out/signableMessage";
-import { Transaction } from "@multiversx/sdk-core/out/transaction";
-import { webviewProviderEventHandler } from "./webviewProviderEventHandler";
+import { SignableMessage } from '@multiversx/sdk-core/out/signableMessage';
+import { Transaction } from '@multiversx/sdk-core/out/transaction';
+import { webviewProviderEventHandler } from './webviewProviderEventHandler';
 import {
   CrossWindowProviderRequestEnums,
   CrossWindowProviderResponseEnums,
-  SignMessageStatusEnum,
-} from "@multiversx/sdk-dapp-utils/out/enums/crossWindowProviderEnums";
+  SignMessageStatusEnum
+} from '@multiversx/sdk-dapp-utils/out/enums/crossWindowProviderEnums';
 import {
   PostMessageParamsType,
   PostMessageReturnType,
-  ReplyWithPostMessagePayloadType,
-} from "@multiversx/sdk-dapp-utils/out/types/crossWindowProviderTypes";
-import { responseTypeMap } from "@multiversx/sdk-dapp-utils/out/constants/crossWindowProviderConstants";
-import { getTargetOrigin } from "./getTargetOrigin";
-import type { IDAppProviderBase } from "@multiversx/sdk-dapp-utils/out/models/dappProviderBase";
+  ReplyWithPostMessagePayloadType
+} from '@multiversx/sdk-dapp-utils/out/types/crossWindowProviderTypes';
+import { responseTypeMap } from '@multiversx/sdk-dapp-utils/out/constants/crossWindowProviderConstants';
+import { getTargetOrigin } from './helpers/getTargetOrigin';
+import { getSafeWindow } from './helpers/getSafeWindow';
+import { getSafeDocument } from './helpers/getSafeDocument';
+import type { IDAppProviderBase } from '@multiversx/sdk-dapp-utils/out/models/dappProviderBase';
 
 interface IWebviewProviderOptions {
   resetStateCallback?: () => void;
 }
-
-const safeWindow = typeof window !== 'undefined' ? window : ({} as any);
 
 export class WebviewProvider implements IDAppProviderBase {
   private static _instance: WebviewProvider;
@@ -32,35 +32,35 @@ export class WebviewProvider implements IDAppProviderBase {
   }
 
   constructor(options?: IWebviewProviderOptions) {
-    if(options?.resetStateCallback) {
+    if (options?.resetStateCallback) {
       this.resetState(options.resetStateCallback);
     }
   }
 
   private resetState = (resetStateCallback?: () => void) => {
-    safeWindow.addEventListener(
-        'message',
-        webviewProviderEventHandler(
-            CrossWindowProviderResponseEnums.resetStateResponse,
-            (data) => {
-              if (
-                  data.type === CrossWindowProviderResponseEnums.resetStateResponse
-              ) {
-                resetStateCallback?.();
+    getSafeWindow().addEventListener?.(
+      'message',
+      webviewProviderEventHandler(
+        CrossWindowProviderResponseEnums.resetStateResponse,
+        (data) => {
+          if (
+            data.type === CrossWindowProviderResponseEnums.resetStateResponse
+          ) {
+            resetStateCallback?.();
 
-                setTimeout(() => {
-                  this.finalizeResetState();
-                }, 500);
-              }
-            }
-        )
+            setTimeout(() => {
+              this.finalizeResetState();
+            }, 500);
+          }
+        }
+      )
     );
   };
 
   init = async () => {
     this.sendPostMessage({
       type: CrossWindowProviderRequestEnums.finalizeHandshakeRequest,
-      payload: undefined,
+      payload: undefined
     });
 
     return true;
@@ -73,7 +73,7 @@ export class WebviewProvider implements IDAppProviderBase {
   logout = async () => {
     const response = await this.sendPostMessage({
       type: CrossWindowProviderRequestEnums.logoutRequest,
-      payload: undefined,
+      payload: undefined
     });
 
     return Boolean(response.payload.data);
@@ -82,19 +82,19 @@ export class WebviewProvider implements IDAppProviderBase {
   relogin = async () => {
     const response = await this.sendPostMessage({
       type: CrossWindowProviderRequestEnums.loginRequest,
-      payload: undefined,
+      payload: undefined
     });
 
     const { data, error } = response.payload;
 
     if (error || !data) {
-      throw new Error("Unable to re-login");
+      throw new Error('Unable to re-login');
     }
 
     const { accessToken } = data;
 
     if (!accessToken) {
-      console.error("Unable to re-login. Missing accessToken.");
+      console.error('Unable to re-login. Missing accessToken.');
       return null;
     }
 
@@ -106,18 +106,18 @@ export class WebviewProvider implements IDAppProviderBase {
   ): Promise<Transaction[] | null> => {
     const response = await this.sendPostMessage({
       type: CrossWindowProviderRequestEnums.signTransactionsRequest,
-      payload: transactionsToSign.map((tx) => tx.toPlainObject()),
+      payload: transactionsToSign.map((tx) => tx.toPlainObject())
     });
 
     const { data: signedTransactions, error } = response.payload;
 
     if (error || !signedTransactions) {
-      console.error("Unable to sign transactions");
+      console.error('Unable to sign transactions');
       return null;
     }
 
     if (response.type == CrossWindowProviderResponseEnums.cancelResponse) {
-      console.warn("Cancelled the transactions signing action");
+      console.warn('Cancelled the transactions signing action');
       this.cancelAction();
       return null;
     }
@@ -130,48 +130,50 @@ export class WebviewProvider implements IDAppProviderBase {
     return response?.[0];
   };
 
-  signMessage = async (message: SignableMessage): Promise<SignableMessage | null> => {
+  signMessage = async (
+    message: SignableMessage
+  ): Promise<SignableMessage | null> => {
     const response = await this.sendPostMessage({
       type: CrossWindowProviderRequestEnums.signMessageRequest,
-      payload: { message: message.message.toString() },
+      payload: { message: message.message.toString() }
     });
 
     const { data, error } = response.payload;
 
     if (error || !data) {
-      console.error("Unable to sign message");
+      console.error('Unable to sign message');
       return null;
     }
 
     if (response.type == CrossWindowProviderResponseEnums.cancelResponse) {
-      console.warn("Cancelled the message signing action");
+      console.warn('Cancelled the message signing action');
       this.cancelAction();
       return null;
     }
 
     if (data.status !== SignMessageStatusEnum.signed) {
-      console.error("Could not sign message");
+      console.error('Could not sign message');
       return null;
     }
 
-    message.applySignature(Buffer.from(String(data.signature), "hex"));
+    message.applySignature(Buffer.from(String(data.signature), 'hex'));
 
     return message;
-  }
+  };
 
   cancelAction = async () => {
     return this.sendPostMessage({
       type: CrossWindowProviderRequestEnums.cancelAction,
-      payload: undefined,
+      payload: undefined
     });
-  }
+  };
 
   finalizeResetState = async () => {
     return this.sendPostMessage({
       type: CrossWindowProviderRequestEnums.finalizeResetStateRequest,
-      payload: undefined,
+      payload: undefined
     });
-  }
+  };
 
   isInitialized = () => true;
 
@@ -180,6 +182,8 @@ export class WebviewProvider implements IDAppProviderBase {
   sendPostMessage = async <T extends CrossWindowProviderRequestEnums>(
     message: PostMessageParamsType<T>
   ): Promise<PostMessageReturnType<T>> => {
+    const safeWindow = getSafeWindow();
+
     if (safeWindow.ReactNativeWebView) {
       safeWindow.ReactNativeWebView.postMessage(
         JSON.stringify(message),
@@ -197,17 +201,23 @@ export class WebviewProvider implements IDAppProviderBase {
     return await this.waitingForResponse(responseTypeMap[message.type]);
   };
 
-  private  waitingForResponse = async <T extends CrossWindowProviderResponseEnums>(
+  private waitingForResponse = async <
+    T extends CrossWindowProviderResponseEnums
+  >(
     action: T
   ): Promise<{
     type: T;
     payload: ReplyWithPostMessagePayloadType<T>;
   }> => {
     return await new Promise((resolve) => {
-      safeWindow.addEventListener(
-          "message",
-          webviewProviderEventHandler(action, resolve)
+      getSafeWindow().addEventListener(
+        'message',
+        webviewProviderEventHandler(action, resolve)
+      );
+      getSafeDocument().addEventListener?.(
+        'message',
+        webviewProviderEventHandler(action, resolve)
       );
     });
-  }
+  };
 }
--- src/webviewProviderEventHandler.ts
+++ src/webviewProviderEventHandler.ts
@@ -1,27 +1,47 @@
-import { getTargetOrigin } from "./getTargetOrigin";
-import { CrossWindowProviderResponseEnums } from "@multiversx/sdk-dapp-utils/out/enums/crossWindowProviderEnums";
-import { ReplyWithPostMessagePayloadType } from "@multiversx/sdk-dapp-utils/out/types/crossWindowProviderTypes";
+import { getTargetOrigin } from './helpers/getTargetOrigin';
+import { CrossWindowProviderResponseEnums } from '@multiversx/sdk-dapp-utils/out/enums/crossWindowProviderEnums';
+import { ReplyWithPostMessagePayloadType } from '@multiversx/sdk-dapp-utils/out/types/crossWindowProviderTypes';
+import { isMobileWebview } from './helpers/isMobileWebview';
+import { getSafeWindow } from './helpers/getSafeWindow';
+import { getSafeDocument } from './helpers/getSafeDocument';
 
-export const webviewProviderEventHandler =
-  <T extends CrossWindowProviderResponseEnums>(
-    action: T,
-    resolve: (value: {
+export type WebviewProviderEventDataType<
+  T extends CrossWindowProviderResponseEnums
+> = {
+  type: T;
+  payload: ReplyWithPostMessagePayloadType<T>;
+};
+
+export const webviewProviderEventHandler = <
+  T extends CrossWindowProviderResponseEnums
+>(
+  action: T,
+  resolve: (value: WebviewProviderEventDataType<T>) => void
+) => {
+  return (event: MessageEvent<WebviewProviderEventDataType<T> | string>) => {
+    let eventData = event.data;
+
+    try {
+      eventData =
+        isMobileWebview() && typeof eventData === 'string'
+          ? JSON.parse(eventData)
+          : eventData;
+    } catch (err) {
+      console.error('error parsing eventData', eventData);
+    }
+
+    const { type, payload } = eventData as {
       type: T;
       payload: ReplyWithPostMessagePayloadType<T>;
-    }) => void
-  ) =>
-  (
-    event: MessageEvent<{
-      type: T;
-      payload: ReplyWithPostMessagePayloadType<T>;
-    }>
-  ) => {
-    const { type, payload } = event.data;
+    };
 
-    if (event.origin != getTargetOrigin()) {
-      console.error("origin not accepted", {
-        eventOrigin: event.origin,
-      });
+    if (isMobileWebview()) {
+      // We need to check the origin inside this if statement to prevent runtime errors.
+      // Accessing event.target.origin from an iframe is restricted due to CORS, but is accessible from the mobile webview.
+      if ((event.target as Window).origin != getSafeWindow().parent?.origin) {
+        return;
+      }
+    } else if (event.origin != getTargetOrigin()) {
       return;
     }
 
@@ -33,9 +53,15 @@ export const webviewProviderEventHandler =
       return;
     }
 
-    window.removeEventListener(
-      "message",
+    getSafeWindow().removeEventListener?.(
+      'message',
       webviewProviderEventHandler(action, resolve)
     );
+    getSafeDocument().removeEventListener?.(
+      'message',
+      webviewProviderEventHandler(action, resolve)
+    );
+
     resolve({ type, payload });
   };
+};
