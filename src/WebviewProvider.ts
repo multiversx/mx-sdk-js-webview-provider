828f30aacd81bb309df0ade8fc5cfad9cd121ad0
Lucas Willems <lcswillems@users.noreply.github.com>
2025-10-13 09:23:26 +0200

    Merge pull request #34 from lcswillems/patch-1
    Iframe should have precedence over rest of alternatives

--- src/WebviewProvider.ts
+++ src/WebviewProvider.ts
@@ -329,15 +329,16 @@ export class WebviewProvider {
   ): Promise<PostMessageReturnType<T>> => {
     const safeWindow = getSafeWindow();
 
-    if (safeWindow.ReactNativeWebView) {
+    const isInIframe = safeWindow.self !== safeWindow.top;
+    if (safeWindow.parent && isInIframe) {
+      safeWindow.parent.postMessage(message, this.allowedOrigin);
+    } else if (safeWindow.ReactNativeWebView) {
       safeWindow.ReactNativeWebView.postMessage(JSON.stringify(message));
     } else if (safeWindow.webkit) {
       safeWindow.webkit.messageHandlers?.jsHandler?.postMessage(
         JSON.stringify(message),
         this.allowedOrigin
       );
-    } else if (safeWindow.parent) {
-      safeWindow.parent.postMessage(message, this.allowedOrigin);
     }
 
     return await this.waitingForResponse(responseTypeMap[message.type]);
