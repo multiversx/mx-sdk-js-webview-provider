87e21593a4fd6a115ca2f398c8b4edba2b5751ab
Lucas Willems <lcswillems@gmail.com>
2025-10-20 17:17:07 +0200

    Introduce getPlatform

--- src/helpers/isMobileWebview.ts
+++ src/helpers/isMobileWebview.ts
@@ -1,7 +1,8 @@
 import { getSafeWindow } from './getSafeWindow';
+import { getPlatform } from './getPlatform';
 
 export const isMobileWebview = () => {
   const safeWindow = getSafeWindow();
   // webkit removed because of false positive detection on iOS Chrome mobile browser
-  return Boolean(safeWindow.ReactNativeWebView); // || safeWindow.webkit;
+  return getPlatform(safeWindow) === "webview";
 };
